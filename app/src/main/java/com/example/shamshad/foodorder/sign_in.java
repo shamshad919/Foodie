ab4fa30ee4871fcb2934caa96fc9455b977b4e5c
shamshad <sshamshad919@gmail.com>
2017-12-26 15:33:48 +0530

    sign in

---
+++ app/src/main/java/com/example/shamshad/foodorder/sign_in.java
@@ -0,0 +1,13 @@
+package com.example.shamshad.foodorder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class sign_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+}
