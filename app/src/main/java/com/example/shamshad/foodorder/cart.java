0dcfc85ce37b3e37f9caed24d44bc3d538d53b6b
firefistace23 <firefistace232323@gmail.com>
2018-02-14 23:02:36 +0530

    cartlistview frontend

--- app/src/main/java/com/example/shamshad/foodorder/cart.java
+++ app/src/main/java/com/example/shamshad/foodorder/cart.java
@@ -1,7 +1,17 @@
 package com.example.shamshad.foodorder;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+
+
+
 
 public class cart extends AppCompatActivity {
 
@@ -9,5 +19,14 @@ public class cart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cart);
+
+        ListView cartlistview=(ListView)findViewById(R.id.cartlistview);
+        String[] foodsincart={"burger","pizza","chicken fry"};
+        String[] priceincart={"50$","100$","2$"};
+
+        ListAdapter mycartlistadapter=new cartlistadapter(this,foodsincart,priceincart);
+        cartlistview.setAdapter(mycartlistadapter);
+
+
     }
 }
