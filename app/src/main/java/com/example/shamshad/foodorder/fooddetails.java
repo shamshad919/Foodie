9baa3809c44bf82d3d771b0dd130ebf3218c5f2b
shamshad <sshamshad919@gmail.com>
2018-02-15 14:11:12 +0530

    Food details Front end

--- app/src/main/java/com/example/shamshad/foodorder/fooddetails.java
+++ app/src/main/java/com/example/shamshad/foodorder/fooddetails.java
@@ -3,11 +3,11 @@ package com.example.shamshad.foodorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class cart extends AppCompatActivity {
+public class fooddetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cart);
+        setContentView(R.layout.fooddetails);
     }
 }
