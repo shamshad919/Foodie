8359cf11cf122e12276ea7d900625543948034fd
shamshad <sshamshad919@gmail.com>
2018-02-27 04:40:33 +0530

    changes

--- app/src/main/java/com/example/shamshad/foodorder/foodList.java
+++ app/src/main/java/com/example/shamshad/foodorder/foodList.java
@@ -1,6 +1,7 @@
 package com.example.shamshad.foodorder;
 
 import android.content.Intent;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,19 +17,25 @@ import com.bumptech.glide.Glide;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.shamshad.foodorder.Interface.ItemClickListener;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
 
 public class foodList extends AppCompatActivity {
-
-
+    private  TextView cart_count;
+    private  Button add_cart;
+    private BottomNavigationView bottomNavigationView;
     private RecyclerView recyclerView;
     private CollapsingToolbarLayout collapsingToolbarLayout;
     private DatabaseReference mRef;
+    private FirebaseAuth mAuth;
     String restaurant_name = "";
     Button quantity;
     ImageView restaurant_image_foodlist;
@@ -40,17 +47,21 @@ public class foodList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.foodview);
 
-        
+        add_cart= (Button) findViewById(R.id.add_cart_button);
+        bottomNavigationView= (BottomNavigationView) findViewById(R.id.bottom_navigation_cart);
         restaurant_name_foodlist = (TextView) findViewById(R.id.name_restaurant_foodlist);
         restaurant_image_foodlist = (ImageView) findViewById(R.id.image_restaurant_foodlist);
         mRef = FirebaseDatabase.getInstance().getReference("restaurants");
         recyclerView = (RecyclerView) findViewById(R.id.recycler_foodview);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        cart_count= (TextView) findViewById(R.id.cart_count);
 
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_foodlist);
         collapsingToolbarLayout.setExpandedTitleColor(R.style.ExpandedAppbar);
         collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.Collapsedappbar);
+
+
 
 
 
@@ -80,12 +91,15 @@ public class foodList extends AppCompatActivity {
             }
         });
 
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user =mAuth.getCurrentUser();
+        final String uid=user.getUid();
         adapter = new FirebaseRecyclerAdapter<food_list_details, food_viewHolder>(food_list_details.class,
                 R.layout.food_listrow,
                 food_viewHolder.class,
                 mRef.child(restaurant_name).child("foodlist")) {
             @Override
-            protected void populateViewHolder(final food_viewHolder viewHolder, final food_list_details model, int position) {
+            protected void populateViewHolder(final food_viewHolder viewHolder, final food_list_details model, final int position) {
                 viewHolder.textView.setText(model.text);
                 viewHolder.priceview.setText(model.price);
                 Glide.with(foodList.this).load(model.image).into(viewHolder.imageView);
@@ -95,8 +109,35 @@ public class foodList extends AppCompatActivity {
                     public void onClick(View v) {
                         viewHolder.add_button.setVisibility(View.GONE);
                         viewHolder.numberButton_foodlist.setVisibility(View.VISIBLE);
+                        viewHolder.numberButton_foodlist.setNumber(String.valueOf(1));
+                        bottomNavigationView.setVisibility(View.VISIBLE);
+                        String count=viewHolder.numberButton_foodlist.getNumber();
+                        cart_count.setText(viewHolder.numberButton_foodlist.getNumber()+" Items");
+                        DatabaseReference cartref=FirebaseDatabase.getInstance().getReference("user").child(uid).child("cart").child(adapter.getRef(position).getKey());
+                        cartref.child("foodid").setValue(adapter.getRef(position).getKey());
+                        cartref.child("quantity").setValue(count);
+                        Toast.makeText(foodList.this, "Added to Cart Successfullly", Toast.LENGTH_SHORT).show();
                     }
                 });
+             viewHolder.numberButton_foodlist.setOnValueChangeListener(new ElegantNumberButton.OnValueChangeListener() {
+                 @Override
+                 public void onValueChange(ElegantNumberButton view, int oldValue, int newValue) {
+
+                     if(viewHolder.numberButton_foodlist.getNumber()==String.valueOf(0)){
+                         viewHolder.add_button.setVisibility(View.VISIBLE);
+                         viewHolder.numberButton_foodlist.setVisibility(View.GONE);
+                         bottomNavigationView.setVisibility(View.GONE);
+                     }
+                     else{
+                        String count=viewHolder.numberButton_foodlist.getNumber();
+                         cart_count.setText(viewHolder.numberButton_foodlist.getNumber()+" Items");
+                         DatabaseReference cartref=FirebaseDatabase.getInstance().getReference("user").child(uid).child("cart").child(adapter.getRef(position).getKey());
+                         cartref.child("foodid").setValue(adapter.getRef(position).getKey());
+                         cartref.child("quantity").setValue(count);
+                         Toast.makeText(foodList.this, "Added to Cart Successfullly", Toast.LENGTH_SHORT).show();
+                     }
+                 }
+             });
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onCLick(View view, int position, boolean isLongCLick) {
